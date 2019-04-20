d8503e98b969bee2cfcf036c845864b9c1f81345
ThePackPater <thepackpater@gmail.com>
2019-04-20 17:15:48 -0500

    too many files updated to mention, i don't think i'll ever get this one

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,5 @@
 // `apiRoutes.js` file contains two routes:
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 //var getFriends = require("../data/friends");
 
 // * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
@@ -11,7 +11,58 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        res.json(newFriend);
-    });
+        var totalDifference = 0;
+        var bestMatch = {
+            name: "",
+            quote: "",
+            photo: "",
+            friendDifference: 1000
+        };
+        var userData = req.body;
+        var userName = userData.name;
+        var userQuote = userData.quote;
+        var userScores = userData.scores;
 
+        var b = userScores.map(function(item) {
+            return parseInt(item, 10);
+        });
+        userData = {
+            name: req.body.name,
+            quote: req.body.quote,
+            photo: req.body.photo,
+            scores: b
+        };
+        console.log("Name: " + userName);
+        console.log("Quote: " + userQuote);
+
+        var sum = b.reduce((a, b) => a + b, 0);
+        console.log("User score " + sum);
+        console.log("Best Match friend diff " + bestMatch.friendDifference);
+        console.log("***************************************************");
+
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friend[i].name);
+            totalDifference = 0;
+            console.log("Total Difference :" + totalDifference);
+            console.log("Best Match Friend Difference: " + bestMatch.friendDifference);
+            var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+            console.log("Total Friend Score: " + bfriendScore);
+            totalDifference += Math.abs(sum - bfriendScore);
+            console.log("--------------------------------> " + totalDifference);
+
+            if (totalDifference <= bestMatch.friendDifference) {
+                bestMatch.name = friends[i].name;
+                bestMatch.quote = friends[i].quote;
+                bestMatch.photo = friends[i].photo;
+                bestMatch.friendDifference = totalDifference;
+            }
+
+            console.log(totalDifference + " Total Difference");
+        }
+        console.log(bestMatch);
+        friends.push(userData);
+        console.log("New User Added");
+        console.log(userData);
+        res.json(bestMatch);
+    });
 };
