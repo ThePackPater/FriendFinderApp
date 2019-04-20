cfd024751f5be9777d915c74637c7f8708d6670a
ThePackPater <thepackpater@gmail.com>
2019-04-20 18:39:31 -0500

    update api routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,6 @@ var friends = [
 
     {
         name: "Obi George Carlin Wan",
-        quote: "Think of how stupid the average person is, and realize half of them are stupider than that.",
         photo: "http://www.freakingnews.com/pictures/43500/George-Carlin-as-Obi-Wan-Kenobi--43670.jpg",
         scores: [
             5,
@@ -21,7 +20,6 @@ var friends = [
 
     {
         name: "Dr Emmit Brown",
-        quote: "GREAT SCOT!!",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsCxezTG0wLXvjcPaVLnGkX3PQWJtJh-_uuSxANZSKNml8bSZleQ",
         scores: [
             4,
@@ -39,7 +37,6 @@ var friends = [
 
     {
         name: "Sad Keanu",
-        quote: "Turns out the matrix was just another computer..",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlDX7kV_hp5cwoWrBdcZwvnLisTOM52cfU_uK7kPHIxbtrALectA",
         scores: [
             3,
@@ -58,7 +55,6 @@ var friends = [
 
     {
         name: "Ruby Rhod",
-        quote: "What's wrong with you? What you screamin' for? Every 5 minutes there's somethin', a bomb or somethin'. I'm leavin'. bzzzz.",
         photo: "http://3.bp.blogspot.com/-catNMcbVyU4/U7dn9oeB_2I/AAAAAAAAFtw/W-OfOP3f44c/s1600/ruby+rhod+the+fifth+element+.jpg",
         scores: [
             2,
@@ -77,7 +73,6 @@ var friends = [
 
     {
         name: "Jar Jar Binks",
-        quote: "Meesa totally not a SITH LORD!",
         photo: "http://1.bp.blogspot.com/-yMm_qUAAzP4/U7tyB5ud-tI/AAAAAAAAFw8/RNC__mPM8_c/s1600/jar+jar+binks+phantom+menance1.jpg",
         scores: [
             1,
@@ -95,7 +90,6 @@ var friends = [
 
     {
         name: "Zaphod Beeblebrox",
-        quote: "If there's anything more important than my ego around, I want it caught and shot now.",
         photo: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-the-hitchhikers-guide-to-the-galaxy-sam-rockwell.jpg",
         scores: [
             5,
@@ -117,5 +111,4 @@ var friends = [
 
 
 //export the array. 
-
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,4 @@
-// `apiRoutes.js` file contains two routes:
 var friends = require("../data/friends.js");
-//var getFriends = require("../data/friends");
-
-// * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-// * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
@@ -14,13 +9,11 @@ module.exports = function(app) {
         var totalDifference = 0;
         var bestMatch = {
             name: "",
-            quote: "",
             photo: "",
             friendDifference: 1000
         };
         var userData = req.body;
         var userName = userData.name;
-        var userQuote = userData.quote;
         var userScores = userData.scores;
 
         var b = userScores.map(function(item) {
@@ -28,31 +21,33 @@ module.exports = function(app) {
         });
         userData = {
             name: req.body.name,
-            quote: req.body.quote,
             photo: req.body.photo,
             scores: b
         };
         console.log("Name: " + userName);
-        console.log("Quote: " + userQuote);
 
-        var sum = b.reduce((a, b) => a + b, 0);
+        var sum = b.reduce(function(total, amount) {
+            return total + amount;
+        });
+
         console.log("User score " + sum);
         console.log("Best Match friend diff " + bestMatch.friendDifference);
         console.log("***************************************************");
 
         for (var i = 0; i < friends.length; i++) {
-            console.log(friend[i].name);
+            console.log(friends[i].name);
             totalDifference = 0;
             console.log("Total Difference :" + totalDifference);
             console.log("Best Match Friend Difference: " + bestMatch.friendDifference);
-            var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+            var bfriendScore = friends[i].scores.reduce(function(total, amount) {
+                return total + amount;
+            });
             console.log("Total Friend Score: " + bfriendScore);
             totalDifference += Math.abs(sum - bfriendScore);
             console.log("--------------------------------> " + totalDifference);
 
             if (totalDifference <= bestMatch.friendDifference) {
                 bestMatch.name = friends[i].name;
-                bestMatch.quote = friends[i].quote;
                 bestMatch.photo = friends[i].photo;
                 bestMatch.friendDifference = totalDifference;
             }
